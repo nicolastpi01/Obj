dd1bfcc1fd406f1cee1f9d8a9abbdb8105d9870f
nicolas <nicolastpi10@gmail.com>
2017-12-11 20:15:18 -0300

    avance en partial function & transformaciones de figuras

--- src/main/scala/dominio/Motor.scala
+++ src/main/scala/dominio/Motor.scala
@@ -8,29 +8,41 @@ trait Figura {
 case class Circulo(x:Int, y: Int, radio: Double)
   extends Figura
 
-case class Rectangulo(x:Int, y: Int, ancho: Int, alto: Int)
+case class Rectangulo(x:Int, y: Int, ancho: Double, alto: Double)
+  extends Figura
+
+case class Linea(x:Int, y: Int, otroX: Int, otroY: Int)
   extends Figura
 
 object Motor {
 
-  def trasladar[T](x: Int, y: Int, figura: T) :Figura = figura match {
-    case Circulo(xPos, yPos, radio) => Circulo(xPos+x, yPos+y, radio)
-    case Rectangulo(xPos, yPos, ancho, alto) => Rectangulo(xPos+x, yPos+y, ancho, alto)
-  }
-/*
-  def mover(x: Int, y: Int) = (figura: Figura) => figura match {
-    case Circulo(xPos, yPos, radio) => Circulo(x, y, radio)
-    case Rectangulo(xPos, yPos, ancho, alto) => Rectangulo(x, y, ancho, alto)
-    //case Linea(xPos, yPos, ancho, alto) => Linea(x, y, ancho, alto)
+  def trasladar[T <: Figura](x: Int, y: Int) (figura: T) :T = {
+    val result = figura match {
+      case Circulo(xPos, yPos, radio) => Circulo(xPos+x, yPos+y, radio)
+      case Rectangulo(xPos, yPos, ancho, alto) => Rectangulo(xPos+x, yPos+y, ancho, alto)
+      case Linea(xPos, yPos, otroX, otroY) => Linea(xPos+x, yPos+y, otroX+x, otroY+y)
+    }
+    result.asInstanceOf[T]
   }
 
-  def escalar(n: Double) = (figura: Figura) => figura match {
-    case Circulo(xPos, yPos, radio) => Circulo(xPos, yPos, n*radio)
-    case Rectangulo(xPos, yPos, ancho, alto) => Rectangulo(xPos, yPos, ancho*2, alto*2)
-    //case Linea(xPos, yPos, ancho, alto) => Linea(x, y, ancho, alto)
+
+  def mover[T <: Figura](x: Int, y: Int) (figura: T): T = {
+    val result = figura match {
+      case Circulo(xPos, yPos, radio) => Circulo(x, y, radio)
+      case Rectangulo(xPos, yPos, ancho, alto) => Rectangulo(x, y, ancho, alto)
+      case Linea(xPos, yPos, otroX, otroY) => Linea(x, y, otroX, otroY)
+    }
+    result.asInstanceOf[T]
   }
 
-*/
+  def escalar[T <: Figura] (n: Double) (figura: T) : T = {
+    val result = figura match {
+      case Circulo(xPos, yPos, radio) => Circulo(xPos, yPos, n*radio)
+      case Rectangulo(xPos, yPos, ancho, alto) => Rectangulo(xPos, yPos, ancho*n, alto*n)
+      //case Linea(xPos, yPos, ancho, alto) => Linea(x, y, ancho, alto)
+    }
+    result.asInstanceOf[T]
+  }
 }
 
 
--- src/test/scala/pruebas/Test.scala
+++ src/test/scala/pruebas/Test.scala
@@ -7,95 +7,93 @@ class Test extends FlatSpec{
   // TRASLADAR
 
   "Un Motor" should "trasladar un Circulo" in {
-    val trasladar: Figura = Motor.trasladar(2, -1, Circulo(1,1,43))
-    val circulo : Circulo = trasladar.asInstanceOf[Circulo]
-    println(circulo)
+    val trasladar = Motor.trasladar[Circulo](2,-1)_
+    val circulo: Circulo = trasladar(Circulo(1,1,43))
 
     assert(circulo.x === 3)
     assert(circulo.y === 0)
     assert(circulo.radio === 43)
   }
-/*
+
   "Un Motor" should "trasladar una linea" in {
-    val trasladarF = Motor.trasladar(2, 2)
-    val lineaBase = Linea(1,1,5,0)
-    val linea = trasladarF(lineaBase)
+    val trasladar = Motor.trasladar[Linea](2, 2)_
+    val linea = trasladar(Linea(1,1,5,1))
 
     assert(linea.x === 3)
     assert(linea.y === 3)
+    assert(linea.otroX === 7)
+    assert(linea.otroY === 3)
   }
-*/
+
   "Un Motor" should "trasladar un Rectangulo" in {
 
-    val trasladar = Motor.trasladar(2, 2, Rectangulo(1,3,5,7))
-    //val rectangulo = trasladar(Rectangulo(1,3,5,7))
+    val trasladar = Motor.trasladar[Rectangulo](2,2)_
+    val rectangulo :Rectangulo = trasladar(Rectangulo(1,3,5,7))
 
-    assert(trasladar.x === 3)
-    assert(trasladar.y === 5)
-    //assert(rectangulo.ancho === 1)
-    //assert(rectangulo.alto === 1)
+    assert(rectangulo.x === 3)
+    assert(rectangulo.y === 5)
+    assert(rectangulo.ancho === 5)
+    assert(rectangulo.alto === 7)
   }
 
   // MOVER
+
+  "Un Motor" should "mover un Rectangulo" in {
+
+    val move = Motor.mover[Rectangulo](2, 2) _
+    val rectangulo = move(Rectangulo(1, 3, 5, 7))
+
+    assert(rectangulo.x === 2)
+    assert(rectangulo.y === 2)
+    assert(rectangulo.ancho === 5)
+    assert(rectangulo.alto === 7)
+  }
+
+  "Un Motor" should "mover un Circulo" in {
+
+    val move = Motor.mover[Circulo](1, 5)_
+    val circulo = move(Circulo(1,0,58))
+
+    assert(circulo.x === 1)
+    assert(circulo.y === 5)
+    assert(circulo.radio === 58)
+  }
+
+  "Un Motor" should "mover una linea" in {
+    val mover = Motor.mover[Linea](2, 2)_
+    val linea = mover(Linea(1,1,5,1))
+
+    assert(linea.x === 2)
+    assert(linea.y === 2)
+    assert(linea.otroX === 5)
+    assert(linea.otroY === 2)
+  }
+
+  // Escalar n
+  "Un Motor" should "escalar un Rectangulo" in {
+
+    val escalar = Motor.escalar[Rectangulo](2)_
+    val rectangulo : Rectangulo = escalar(Rectangulo(1,3,4,6))
+
+    assert(rectangulo.alto === 12)
+    assert(rectangulo.ancho === 8)
+  }
   /*
-    "Un Motor" should "mover un Rectangulo" in {
+            "Un Motor" should "escalar un Circulo" in {
 
-      val Fmover = Motor.mover(2, 2)
-      val rectangulo = Fmover(Rectangulo(1,3,5,7))
+              val Fescalar = Motor.escalar(0.5)
+              val circulo = Fescalar(Circulo(1,1,50))
 
-      assert(rectangulo.x === 2)
-      assert(rectangulo.y === 2)
-      //assert(rectangulo.ancho === 1)
-      //assert(rectangulo.alto === 1)
-    }
+              //assert(circulo.radio === 25)
+            }
 
-    "Un Motor" should "mover un Circulo" in {
+              "Un Motor" should "escalar una Linea" in {
 
-      val Fmover = Motor.mover(1, 5)
-      val cuadrado = Fmover(Circulo(1,0,58))
+                val Fmover = Motor.mover(1, 5)
+                val linea = Fmover(Linea(1,1,5,0))
 
-      assert(cuadrado.x === 1)
-      assert(cuadrado.y === 5)
-      //assert(rectangulo.ancho === 1)
-      //assert(rectangulo.alto === 1)
-    }
-
-    "Un Motor" should "mover una linea" in {
-      val moverF = Motor.mover(2, 2)
-      val lineaBase = Linea(1,1,5,0)
-      val linea = moverF(lineaBase)
-
-      assert(linea.x === 2)
-      assert(linea.y === 2)
-    }
-
-    // Escalar n
-
-    "Un Motor" should "escalar un Rectangulo" in {
-
-      val Fescalar = Motor.escalar(2)
-      val rectangulo = Fescalar(Rectangulo(1,3,4,6))
-
-      //assert(rectangulo.alto === 12)
-      //assert(rectangulo.ancho === 8)
-      //assert(rectangulo.x === 1) # No se modif
-    }
-
-    "Un Motor" should "escalar un Circulo" in {
-
-      val Fescalar = Motor.escalar(0.5)
-      val circulo = Fescalar(Circulo(1,1,50))
-
-      //assert(circulo.radio === 25)
-    }
-
-    "Un Motor" should "escalar una Linea" in {
-
-      val Fmover = Motor.mover(1, 5)
-      val linea = Fmover(Linea(1,1,5,0))
-
-      //assert(linea.x === 1)
-      //assert(linea.y === 5)
-    }
-  */
+                //assert(linea.x === 1)
+                //assert(linea.y === 5)
+              }
+            */
 }
