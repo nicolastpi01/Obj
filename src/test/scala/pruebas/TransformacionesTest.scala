27d92aa7f19774001433726568813bfd13afac55
nicolas <nicolastpi10@gmail.com>
2017-12-18 08:13:59 -0300

    transformacionesTest comentado en ingles

--- src/test/scala/pruebas/TransformacionesTest.scala
+++ src/test/scala/pruebas/TransformacionesTest.scala
@@ -6,7 +6,7 @@ import org.scalatest.FlatSpec
 class TransformacionesTest extends FlatSpec{
   // TRASLADAR
 
-  "Un Motor" should "trasladar un Circulo" in {
+  "A Transform object" should "trasladar a Circle" in {
     val trasladar = Transform.trasladar[Circulo](2,-1)_
     val circulo : Circulo = Circulo(1,1,43)
     val circuloNuevo: Circulo = trasladar(circulo)
@@ -17,7 +17,7 @@ class TransformacionesTest extends FlatSpec{
     assert(circulo !== circuloNuevo)
   }
 
-  "Un Motor" should "trasladar una linea" in {
+  "A Transform object" should "trasladar a line" in {
     val trasladar = Transform.trasladar[Linea](2, 2)_
     val linea = trasladar(Linea(1,1,0,3))
 
@@ -28,7 +28,7 @@ class TransformacionesTest extends FlatSpec{
   }
 
 
-  "Un Motor" should "trasladar un Rectangulo" in {
+  "A Transform object" should "trasladar a Rectangle" in {
 
     val trasladar = Transform.trasladar[Rectangulo](2,2)_
     val rectangulo :Rectangulo = trasladar(Rectangulo(1,3,5,7))
@@ -42,7 +42,7 @@ class TransformacionesTest extends FlatSpec{
 
   // MOVER
 
-  "Un Motor" should "mover un Rectangulo" in {
+  "A Transform object" should "move a Rectangle" in {
 
     val move = Transform.mover[Rectangulo](2, 2) _
     val rectangulo = move(Rectangulo(1, 3, 5, 7))
@@ -54,7 +54,7 @@ class TransformacionesTest extends FlatSpec{
   }
 
 
-  "Un Motor" should "mover un Circulo" in {
+  "A Transform object" should "move a Circle" in {
 
     val move = Transform.mover[Circulo](1, 5)_
     val circulo = move(Circulo(1,0,58))
@@ -65,7 +65,7 @@ class TransformacionesTest extends FlatSpec{
   }
 
 
-  "Un Motor" should "mover una linea" in {
+  "A Transform object" should "move a line" in {
     val mover = Transform.mover[Linea](2, 2)_
     val linea = mover(Linea(1,1,-5,0))
 
@@ -76,8 +76,7 @@ class TransformacionesTest extends FlatSpec{
   }
 
 
-  // Escalar n
-  "Un Motor" should "escalar un Rectangulo" in {
+  "A Transform object" should "escalar a Rectangle" in {
 
     val escalar = Transform.escalar[Rectangulo](2)_
     val rectangulo : Rectangulo = escalar(Rectangulo(1,3,4,6))
@@ -87,7 +86,7 @@ class TransformacionesTest extends FlatSpec{
   }
 
 
-  "Un Motor" should "escalar un Circulo" in {
+  "A Transform object" should "escalar a Circle" in {
 
     val escalar = Transform.escalar[Circulo](0.5)_
     val circulo :Circulo = escalar(Circulo(1,1,50))
@@ -96,7 +95,7 @@ class TransformacionesTest extends FlatSpec{
   }
 
 
-  "Un Motor" should "escalar una Linea" in {
+  "A Transform object" should "escalar a Line" in {
 
     val escalar = Transform.escalar[Linea](2)_
     val linea = escalar(Linea(1,1,5,0))
@@ -104,6 +103,5 @@ class TransformacionesTest extends FlatSpec{
     assert(linea.otroX === 10)
     assert(linea.otroY === 0)
   }
-
 
 }
